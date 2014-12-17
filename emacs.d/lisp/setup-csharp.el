633e000b88ef1f311c7541cbfcdf995c938b8e21
Ambrose Krapacs <ambrose@backflipstudios.com>
2014-12-17 11:26:25 -0700

    Fixed csharp mode

--- emacs.d/lisp/setup-csharp.el
+++ emacs.d/lisp/setup-csharp.el
@@ -1,13 +1,6 @@
-(require 'csharp-mode)
+(defun my-csharp-style ()
+  (local-set-key (kbd "{") 'c-electric-brace))
 
-(autoload 'csharp-mode "csharp-mode" "Major mode for editing C# code." t)
-(setq auto-mode-alist
-      (append '(("\\.cs$" . csharp-mode)) auto-mode-alist))
-
-(defun my-csharp-mode-fn ()
-  (require 'flymake)
-  (flymake-mode 1))
-
-(add-hook 'csharp-mode-hook 'my-csharp-mode-fn t)
+(add-hook 'csharp-mode-hook 'my-csharp-style)
 
 (provide 'setup-csharp)
